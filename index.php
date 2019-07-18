1d9182d0a48502b5a89b446d64d5cd9ddb21b4c9
GTH-pzu <53040275+GTH-pzu@users.noreply.github.com>
2019-07-18 14:58:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ Já jsem speciální stránka. Vím, že právě je <?echo Date("G:i")?>
 </HTML>
 <br>
 <br>
-<?phpinfo();?>
+  # <?phpinfo();?>
 <br>
 <br>
 <?php
@@ -22,4 +22,19 @@ Ještě se na to budu muset podívat.
 
 <?
 # skriptu je úplně jedno, co tu bude
+?>
+
+<?
+//logický typ
+$mam_malo_penez = TRUE;
+//celočíselný typ
+$plat = 10000;
+//desetinné číslo
+$disketa = 3.5;
+//řetězec
+$prohlaseni = "Právě se vracím z pražského hradu! ...";
+echo $mam_malo_penez."<BR>";
+echo $plat."<BR>";
+echo $disketa."<BR>";
+echo $prohlaseni."<BR>";
 ?>
