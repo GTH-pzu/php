d33a9bc67247dfceacea1b41cc929217a74043e3
GTH-pzu <53040275+GTH-pzu@users.noreply.github.com>
2019-07-18 16:34:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -38,6 +38,33 @@ echo $plat."<BR>";
 echo $disketa."<BR>";
 echo $prohlaseni."<BR>";
 ?>
+
+<?
+$muj_string="Pepa";
+$muj_integer=2;
+$muj_float=3.5;
+$muj_boolean=FALSE;
+// pokusíme se nějak přetypovat každou proměnnou...
+echo "Typ proměnné muj_string byl <B>".gettype($muj_string)."</B>";
+echo " a hodnota byla <B>".$muj_string."</B>. ";
+settype($muj_string, "boolean");
+echo "Po změně na <B>".gettype($muj_string)."</B> je hodnota <B>".$muj_string."</B>.<BR>\n";
+
+echo "Typ proměnné muj_integer byl <B>".gettype($muj_integer)."</B>";
+echo " a hodnota byla <B>".$muj_integer."</B>. ";
+settype($muj_integer, "float");
+echo "Po změně na <B>".gettype($muj_integer)."</B> je hodnota <B>".$muj_integer."</B>.<BR>\n";
+
+echo "Typ proměnné muj_float byl <B>".gettype($muj_float)."</B>";
+echo " a hodnota byla <B>".$muj_float."</B>. ";
+settype($muj_float, "integer");
+echo "Po změně na <B>".gettype($muj_float)."</B> je hodnota <B>".$muj_float."</B>.<BR>\n";
+
+echo "Typ proměnné muj_boolean byl <B>".gettype($muj_boolean)."</B>";
+echo " a hodnota byla <B>".$muj_boolean."</B>. ";
+settype($muj_boolean, "integer");
+echo "Po změně na <B>".gettype($muj_boolean)."</B> je hodnota <B>".$muj_boolean."</B>.<BR>\n";
+?>
 <br>
 <?
 $fronta[1]="Petr";
