f8601b7ba906151cd6447897b82895e02fe7fd17
GTH-pzu <53040275+GTH-pzu@users.noreply.github.com>
2019-07-19 15:16:24 +0200

    Create Kap16.php

---
+++ Kap16.php
@@ -0,0 +1,18 @@
+<?
+function PocetDnu ($mesic, $rok)
+{
+  return cal_days_in_month(CAL_GREGORIAN, $mesic, $rok);
+}
+?>
+
+<TABLE>
+<TR><TD>Číslo</TD><TD>Druhá mocnina</TD></TR>
+<? $i=1; while ($i<=20):?>
+    <TR><TD><?echo $i?></TD>
+    <TD><?echo $i*$i?></TD></TR>
+<?  $i++; endwhile; ?>
+</TABLE>
+<br>
+<HTML> 
+Já jsem speciální stránka. Vím, že právě je <?echo Date("G:i")?>
+</HTML>
